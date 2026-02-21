3e63122d555597d4e126aacbbf488e8f4180ffc4
RuhanR-git <ruhan.raghu@gmail.com>
2026-02-20 19:27:56 -0700

    Strafing added, PID values changed, physical properties updated, and example code removed.

--- yagsl swerve/src/main/java/frc/robot/RobotContainer.java
+++ yagsl swerve/src/main/java/frc/robot/RobotContainer.java
@@ -1,94 +1,50 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.SwerveSubsystem;
-import swervelib.SwerveInputStream;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.SwerveSubsystem;
+import swervelib.SwerveInputStream;
 
-/**
- * This class is where the bulk of the robot should be declared. Since
- * Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in
- * the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of
- * the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+
   private final SwerveSubsystem driveBase = new SwerveSubsystem();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
+  // Driver Controller on Port 0
   private final CommandXboxController m_driverController = new CommandXboxController(
       OperatorConstants.kDriverControllerPort);
 
-  /**
-   * The container for the robot. Contains subsystems, OI devices, and commands.
-   */
   public RobotContainer() {
-    // Configure the trigger bindings
     configureBindings();
-    driveBase.setDefaultCommand(driveFieldOrientedAngularVelocity);
+
+    // SETUP THE DRIVE STRATEGY
+    // This creates a "Stream" of inputs from your joystick to the robot
+    SwerveInputStream driveInputStream = SwerveInputStream.of(
+        driveBase.getSwerveDrive(),
+        // LEFT STICK Y: Moves robot Forward/Backward
+        () -> m_driverController.getLeftY() * -1, 
+        // LEFT STICK X: Moves robot Left/Right (Strafing)
+        () -> m_driverController.getLeftX() * -1) 
+        // RIGHT STICK X: "Steering" - Makes wheels form a circle to rotate the robot
+        .withControllerRotationAxis(() -> m_driverController.getRightX() * -1) 
+        .deadband(OperatorConstants.DEADBAND)
+        .scaleTranslation(0.8) // 80% speed for safety, change to 1.0 for full power
+        .allianceRelativeControl(true); // Field-Oriented: "Forward" is always away from you
+
+    // Apply the drive command as the default
+    driveBase.setDefaultCommand(driveBase.driveFieldOriented(driveInputStream));
   }
 
-  SwerveInputStream driveAngularVelocity = SwerveInputStream.of(
-      driveBase.getSwerveDrive(),
-      () -> m_driverController.getLeftY() * -1,
-      () -> m_driverController.getLeftX() * -1)
-      .withControllerRotationAxis(m_driverController::getRightX)
-      .deadband(OperatorConstants.DEADBAND)
-      .scaleTranslation(0.8)
-      .allianceRelativeControl(true);
-
-  SwerveInputStream driveDirectAngle = driveAngularVelocity.copy()
-      .withControllerHeadingAxis(m_driverController::getRightX,
-          m_driverController::getRightY)
-      .headingWhile(true);
-
-  Command driveFieldOrientedDirectAngle = driveBase.driveFieldOriented(driveDirectAngle);
-  Command driveFieldOrientedAngularVelocity = driveBase.driveFieldOriented(driveAngularVelocity);
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be
-   * created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
-   * an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
-   * {@link
-   * CommandXboxController
-   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or
-   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is
-    // pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    // DIAGRAM HELP: If the robot's "Forward" gets confused, press the 'Y' button
+    // to reset the gyro so "Forward" is wherever the robot is currently facing.
+    m_driverController.y().onTrue(driveBase.zeroGyroCommand());
+    
+    // Safety: Press 'B' to lock wheels in an 'X' to stop movement
+    m_driverController.b().whileTrue(driveBase.lockPoseCommand());
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
+    return null; // Add auto logic here later
   }
 }
--- yagsl swerve/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ yagsl swerve/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -1,95 +1,51 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
-
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
 
 import java.io.File;
 import java.util.function.Supplier;
 
-import edu.wpi.first.wpilibj.Filesystem;
-import swervelib.parser.SwerveParser;
-import swervelib.SwerveDrive;
-import swervelib.SwerveInputStream;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.util.Units;
-
-import static edu.wpi.first.units.Units.Meter;
-
-
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import swervelib.SwerveDrive;
+import swervelib.parser.SwerveParser;
 
 public class SwerveSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
   
-  File directory = new File(Filesystem.getDeployDirectory(),"swerve");
-  SwerveDrive  swerveDrive;
+  private final SwerveDrive swerveDrive;
+
   public SwerveSubsystem() { 
-    try
-    {
-      swerveDrive = new SwerveParser(directory).createSwerveDrive(Constants.maxSpeed, new Pose2d(new Translation2d(Meter.of(1),
-                                                                                                                  Meter.of(4)),
-                                                                                                                  Rotation2d.fromDegrees(0)));
-      // Alternative method if you don't want to supply the conversion factor via JSON files.
-      // swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed, angleConversionFactor, driveConversionFactor);
-    } catch (Exception e)
-    {
+    File directory = new File(Filesystem.getDeployDirectory(), "swerve");
+    try {
+      // Initialize the drive base using your JSON files
+      swerveDrive = new SwerveParser(directory).createSwerveDrive(Constants.maxSpeed);
+    } catch (Exception e) {
       throw new RuntimeException(e);
     }
   }
 
-  /**
-   * Example command factory method.
-   *
-   * @return a command
-   */
-  public Command exampleMethodCommand() {
-    // Inline construction of command goes here.
-    // Subsystem::RunOnce implicitly requires `this` subsystem.
-    return runOnce(
-        () -> {
-          /* one-time action goes here */
-        });
+  // The command used in RobotContainer to actually move the wheels
+  public Command driveFieldOriented(Supplier<ChassisSpeeds> velocity) {
+    return run(() -> swerveDrive.driveFieldOriented(velocity.get()));
   }
 
-  /**
-   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
-   *
-   * @return value of some boolean subsystem state, such as a digital sensor.
-   */
-  public boolean exampleCondition() {
-    // Query some boolean state, such as a digital sensor.
-    return false;
+  // Resets the Gyro (Essential for Field Oriented Driving)
+  public Command zeroGyroCommand() {
+    return runOnce(() -> swerveDrive.zeroGyro());
+  }
+
+  // Forces wheels into an 'X' shape so the robot can't be pushed
+  public Command lockPoseCommand() {
+    return run(() -> swerveDrive.lockPose());
+  }
+
+  public SwerveDrive getSwerveDrive() {
+    return swerveDrive;
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
-  }
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
-  }
-
-  public SwerveDrive getSwerveDrive() {
-    // TODO Auto-generated method stub
-    return swerveDrive;
-  }
-
-  public void driveFieldOriented(ChassisSpeeds velocity) {
-    swerveDrive.driveFieldOriented(velocity);
-  }
-
-  public Command driveFieldOriented(Supplier<ChassisSpeeds> velocity){
-    return run(() -> {
-      swerveDrive.driveFieldOriented(velocity.get());
-    });
+    // YAGSL handles odometry updates internally
   }
 }
